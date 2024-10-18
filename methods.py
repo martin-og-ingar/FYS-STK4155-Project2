281d6b23a7f6c63414936ee06568597c6ca61ecf
Martin <70632679+martinbolle@users.noreply.github.com>
2024-10-18 10:07:35 +0200

    Added the FrankeFunction for partB

--- methods.py
+++ methods.py
@@ -948,3 +948,11 @@ def adam_sgd_momentum_ridge(
             prev_loss = mse
 
     return beta, iterations, epochs
+
+
+def franke_function(x, y):
+    term1 = 0.75 * np.exp(-(0.25 * (9 * x - 2) ** 2) - 0.25 * ((9 * y - 2) ** 2))
+    term2 = 0.75 * np.exp(-((9 * x + 1) ** 2) / 49.0 - 0.1 * (9 * y + 1))
+    term3 = 0.5 * np.exp(-((9 * x - 7) ** 2) / 4.0 - 0.25 * ((9 * y - 3) ** 2))
+    term4 = -0.2 * np.exp(-((9 * x - 4) ** 2) - (9 * y - 7) ** 2)
+    return term1 + term2 + term3 + term4
